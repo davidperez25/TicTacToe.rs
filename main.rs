49e39f53d6b3f42a4c0a77b6fe01fc87d8d29c31
David Perez <77497008+davidperez25@users.noreply.github.com>
2024-07-08 20:26:57 -0400

    Pretty much finished the game

--- main.rs
+++ main.rs
@@ -53,20 +53,51 @@ impl Grid {
             None => 0,
         };
         
-        if number < self.rows && cols < self.cols {
+        if number < self.rows && cols < self.cols && self.grid[number][cols] == 2 {
             self.grid[number][cols] = 1;
             self.ai();
         } else {
-            println!("Coordinates out of bounds...");
+            println!("Cannot place there...");
         }
     }
     
     fn check(&self) -> i32 {
+        let size = self.grid.len();
+
+        for i in 0..size {
+            if self.grid[i][0] == 1 && self.grid[i][1] == 1 && self.grid[i][2] == 1 {
+                return 1;
+            }
+            if self.grid[i][0] == 0 && self.grid[i][1] == 0 && self.grid[i][2] == 0 {
+                return 0;
+            }
+        }
+
+        for j in 0..size {
+            if self.grid[0][j] == 1 && self.grid[1][j] == 1 && self.grid[2][j] == 1 {
+                return 1;
+            }
+            if self.grid[0][j] == 0 && self.grid[1][j] == 0 && self.grid[2][j] == 0 {
+                return 0;
+            }
+        }
+
+        if self.grid[0][0] == 1 && self.grid[1][1] == 1 && self.grid[2][2] == 1 ||
+            self.grid[0][2] == 1 && self.grid[1][1] == 1 && self.grid[2][0] == 1 {
+            return 1;
+        }
+        if self.grid[0][0] == 0 && self.grid[1][1] == 0 && self.grid[2][2] == 0 ||
+            self.grid[0][2] == 0 && self.grid[1][1] == 0 && self.grid[2][0] == 0 {
+            return 0;
+        }
+        
         for row in &self.grid {
             for &cell in row {
+            
                 if cell == 2 {
-                    return 1;
+                    return 4;
                 }
+                
             }
         }
         0
@@ -124,10 +155,22 @@ impl fmt::Display for Grid {
 fn gloop(mut game: Grid) {
     println!("{}", game);
     
-    if game.check() == 0 {
-        println!("Game over!");
-        
-        return;
+    match game.check() {
+        0 => {
+            println!("X wins!");
+            return;
+        }
+        1 => {
+            println!("O wins!");
+            return;
+        },
+        3 => {
+            println!("Draw!");
+            return;
+        },
+        _ => {
+            
+        },
     }
 
     println!("Enter coordinates (e.g., 'a1', 'b2', etc.): ");
